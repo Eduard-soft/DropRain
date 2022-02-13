00f1ad312eb2d937b3db54463647f02d2017b342
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2022-02-13 13:07:20 +0300

    Initial Commit

---
+++ DropRain/DropRainApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropRainApp.swift
+//  DropRain
+//
+//  Created by Эдуард on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DropRainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
